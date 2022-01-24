8bfe6d1ed508af2f775ba9d3ea01b2c8dfbc21f3
jareddodson <jared.dodson04@gmail.com>
2022-01-24 16:38:22 -0600

    added about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,6 +4,7 @@ function About() {
   return (
     <section>
       <h1 id="about">Who am I?</h1>
+      <p id='about-paragraph'>I am an aspiring software engineer who recently got out of the military and am excited for my coding journey!</p>
     </section>
   );
 }
